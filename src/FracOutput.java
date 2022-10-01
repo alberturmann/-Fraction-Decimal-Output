d68b60aa0bb2fa2f93ff51f0fa3284a2cc1d4399
Albert <113330697+alberturmann@users.noreply.github.com>
2022-10-01 20:36:36 +0200

    Add files via upload

---
+++ src/FracOutput.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+public class FracOutput {
+    public static void main(String args[]){
+String input;
+float n, d;
+
+input = JOptionPane.showInputDialog("Geben Sie den numerator of fraction a ");
+n = Float.parseFloat(input);
+
+input = JOptionPane.showInputDialog("Enter the denominator of fraction a ");
+d = Float.parseFloat(input);
+
+Fraction a = new Fraction(n, d);
+
+input = JOptionPane.showInputDialog("Enter the numerator of fraction b ");
+n = Float.parseFloat(input);
+
+input = JOptionPane.showInputDialog("Enter the denominator of fraction b ");
+d = Float.parseFloat(input);
+
+Fraction b = new Fraction(n, d);
+
+System.out.println("Fraction a in decimal form:"   +a.devide(a.numerator,a.denumerator));
+System.out.println("Fraction b in decimal form:"   +b.devide(b.numerator,b.denumerator));
+
+JOptionPane.showMessageDialog(null, a.devide(a.numerator,a.denumerator));
+JOptionPane.showMessageDialog(null, b.devide(b.numerator,b.denumerator));
+    }
+}
---
+++ src/Fraction.java
@@ -0,0 +1,20 @@
+public class Fraction {
+    float numerator;
+     float denumerator;
+
+        Fraction (float n,float d){
+            this.numerator=n;
+            this.denumerator=d;  
+    } 
+
+   float devide(float n,float d)
+   {
+    numerator=n;
+    denumerator=d;  
+    float result;
+    result = n/d;
+    return result;
+
+   }
+
+}
